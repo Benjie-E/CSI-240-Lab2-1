3fe641fb95bf4e1d85273cca6c0f194fbf175ec4
Benjie-E <90060865+Benjie-E@users.noreply.github.com>
2022-01-28 16:52:36 -0500

    working functions and output

---
+++ src/book.cpp
@@ -0,0 +1,95 @@
+/*
+Author:       Benjie
+Class:        CSI-240-03
+Assignment:   Lab 3
+Date Assigned:1/24/2022
+Due Date:     2/1/2022 14:30
+Description:
+A brief description of the purpose of the program.
+Certification of Authenticity:
+    I certify that this is entirely my own work, except where I have given
+    fully-documented references to the work of others. I understand the
+    definition and consequences of plagiarism and acknowledge that the assessor
+    of this assignment may, for the purpose of assessing this assignment:
+        - Reproduce this assignment and provide a copy to another member of
+academic staff; and/or
+        - Communicate a copy of this assignment to a plagiarism checking
+          service (which may then retain a copy of this assignment on its
+          database for the purpose of future plagiarism checking)
+*/
+#include <fstream>
+#include <iostream>
+
+#include "book.h"
+const float OZ_IN_POUNDS = 16.0;
+
+#include <sstream>
+Book::Book() {
+	bName = "";
+	bType = Type::UNKNOWN;
+	bPages = 0;
+	bOunces = 0.0f;
+}
+
+Book::Book(const string &name, Type type, int pages, float ounces) {
+	bName = name;
+	bType = type;
+	bPages = pages;
+	bOunces = ounces;
+}
+
+string Book::formatReportLine() {
+	stringstream buffer("");
+	buffer << getName() << " | Type: " << getTypeName() << " | Pages: " \
+			<< getPages() << " | Weight(lbs): " << getWeightLbs()\
+			<<"Average stars:"<< calculateAverageReview()<< "| Most frequent review: "<<getMostFrequentReview();
+	return buffer.str();
+}
+
+float Book::getWeightLbs() {
+	return getOunces()*OZ_IN_POUNDS;
+}
+
+string Book::getTypeName() {
+	return TYPE_WORDS[bType];
+}
+
+void Book::addReviews(const string &reviewFile) {
+		ifstream input("files/"+reviewFile);
+		int* reviewArray = bReviews;
+		string buffer;
+		if( input.good() )
+		{
+			for(int i=0;!input.eof()&&i<RATING_COUNT;i++){
+				getline( input, buffer );
+				reviewArray[i]=stoi(buffer);
+				//write out report line for movie here!
+
+			}
+		}
+		input.close();
+}
+
+float Book::calculateAverageReview() {
+	float total{0};//sum of review scores
+	int quantity{0};//amount of reviews
+	for(int i=0;i<RATING_COUNT;i++){
+		quantity+=bReviews[i];
+		total+=(bReviews[i])*(RATING_COUNT-i);
+	}
+	cout <<"total:"<<total<<"quantity:"<<quantity<<endl;
+	return total/quantity;
+}
+
+int Book::getMostFrequentReview() {
+	int max{-1}; //most frequent amount
+	int freq{-1};//most frequent rating
+	for(int i=0;i<RATING_COUNT;i++){
+			if(max<bReviews[i]){
+				max=bReviews[i];
+				freq=RATING_COUNT-i;
+			}
+
+	}
+	return freq;
+}
---
+++ src/book.h
@@ -0,0 +1,61 @@
+//
+//  book.h
+//  lab3
+//
+//
+//  Modified by: Murat K. Gungor 
+//  Thanks to Brian R. Hall
+//
+
+#ifndef __lab3__book__
+#define __lab3__book__
+
+#include <string>
+
+using namespace std;
+const int RATING_COUNT = 5;
+enum Type
+{
+	UNKNOWN = -1,
+   PAPERBACK,
+	HARDBACK
+};
+
+const string TYPE_WORDS[] = { "Paperback", "Hardback" };
+
+class Book
+{
+public:
+	//default constructor - not actually used but should be implemented anyway
+	Book();
+	//constructor
+	Book( const string& name, Type type, int pages, float ounces );
+	//destructor
+	~Book(){};
+   
+	string formatReportLine();  //return a string with all the info for the book
+	float getWeightLbs();  //calculate and return the weight of the book in lbs
+	string getTypeName();  //return the string which correlates with the book type
+   
+	//accessors
+	string getName(){ return bName; };
+	Type getType(){ return bType; };
+	int getPages(){ return bPages; };
+	float getOunces(){ return bOunces; };
+
+	void addReviews( const string& reviewFile );
+	//calculate the average of the reviews
+	float calculateAverageReview();
+	//get the review (number of stars) with the highest count
+	int getMostFrequentReview();
+   
+private:
+	string bName;  //name of the book
+	Type bType;  //the type of book (Type is an enumerated type)
+	int bPages;  //how many pages the book contains
+	float bOunces;  //how much the book weighs in ounces
+	int bReviews[RATING_COUNT];
+};
+
+
+#endif /* defined(__lab3__book__) */
